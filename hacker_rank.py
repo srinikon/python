ec864244cd8cbbb992d134fb9fd353e51f5653e9
srinikon <skondepudi@gmail.com>
2017-12-02 21:24:20 -0800

    Remove duplicates from linked lists

--- hacker_rank.py
+++ hacker_rank.py
@@ -1,16 +1,15 @@
-"""
- Find Merge Point of Two Lists
 
- Find the node at which both lists merge and return the data of that node.
- head could be None as well for empty list
- Node is defined as
+ # Find Merge Point of Two Lists
+
+ # Find the node at which both lists merge and return the data of that node.
+ # head could be None as well for empty list
+ # Node is defined as
  
- class Node(object):
+ # class Node(object):
  
-   def __init__(self, data=None, next_node=None):
-       self.data = data
-       self.next = next_node
-"""
+ #   def __init__(self, data=None, next_node=None):
+ #       self.data = data
+ #       self.next = next_node
 
 def FindMergeNode(headA, headB):
     while(headA):
@@ -23,3 +22,30 @@ def FindMergeNode(headA, headB):
                 return(headB_copy.data);
             headB_copy = headB_copy.next        
         headA = headA.next
+
+
+ # Delete duplicate-value nodes from a sorted linked list
+ # head could be None as well for empty list
+ # Node is defined as
+ 
+ # class Node(object):
+ 
+ #   def __init__(self, data=None, next_node=None):
+ #       self.data = data
+ #       self.next = next_node
+
+ # return back the head of the linked list in the below method.
+
+
+def RemoveDuplicates(head):
+    h = Node(head.data)
+    n = h
+    while(head and head.next):
+        if (head.data == head.next.data):
+            head = head.next
+            continue
+        else:
+            head = head.next
+            n.next = Node(head.data)
+            n = n.next
+    return(h)
