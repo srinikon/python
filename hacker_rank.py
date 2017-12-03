c82eee9e456429e558a67cc5f5e76a3c28a9f200
srinikon <skondepudi@gmail.com>
2017-12-02 22:55:56 -0800

    List

--- hacker_rank.py
+++ hacker_rank.py
@@ -49,3 +49,29 @@ def RemoveDuplicates(head):
             n.next = Node(head.data)
             n = n.next
     return(h)
+
+
+# Lists
+if __name__ == '__main__':
+    N = int(input())
+    cmd = []
+    list = []
+    for i in range (0, N):
+        cmd.append(str(input()))
+        
+    for i in range(0, N):
+        temp = cmd[i].split(" ")
+        if(temp[0] == 'insert'):
+            list.insert(int(temp[1]), int(temp[2]))
+        elif(temp[0] == 'print'):
+            print(list)
+        elif(temp[0] == 'remove'):
+            list.remove(int(temp[1]))
+        elif(temp[0] == 'append'):
+            list.append(int(temp[1]))
+        elif(temp[0] == 'sort'):
+            list.sort()
+        elif(temp[0] == 'pop'):
+            list.pop()
+        elif(temp[0] == 'reverse'):
+            list.reverse()
